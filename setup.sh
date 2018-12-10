1cd949a3da395de55b25de829048c36148e79fe6
ashokkumar11 <ashokkumar11595@gmail.com>
2018-12-10 18:12:21 +0530

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+source ~/setup-env-ci.sh
+source ~/setup-env-manual.sh
+source ~/setup-username-passwd.sh
+bash ~/mount-zdrive.sh
+bash ~/config-svn.sh
+bash ~/config-bower.sh
+bash ~/config-pip.sh
+bash ~/create-jenkins-mk.sh
